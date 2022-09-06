25ce71c07d03d1c21709f7671c4a6a764cf0b33b
十音 <908296267@qq.com>
2022-09-06 12:07:01 +0800

    inint

---
+++ 手写/发布-订阅者模式.ts
@@ -0,0 +1,28 @@
+class Observe {
+    message
+    constructor() {
+        this.message = {};
+    }
+    $on(type:string, callback):void {
+        if (!this.message[type]) {
+            this.message[type] = [];
+        }
+        this.message[type].push(callback)
+    }
+    $off(type:string, callback):void {
+        if (!callback)
+            delete this.message[type];
+        else
+            this.message[type] = this.message[type].filter((item) => item !== callback);
+    }
+    $emit(type:string):void { 
+        this.message[type].forEach(key=>key())
+    }
+}
+
+const ob = new Observe();
+function call1() { console.log('111') }
+function call2() { console.log('2222') }
+ob.$on('buy', call1)
+ob.$on('buy', call2)
+ ob.$emit('buy')
